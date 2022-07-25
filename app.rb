de6e65fb0ce061d3813bafa2389999cd2de0a1cc
JaiVon <frederickjaivon@yahoo.com>
2022-07-25 02:18:41 -0400

    commit tree 7b8fc20c1e55cff3249dc3bbc6100e51c1a36c05

---
+++ app.rb
@@ -0,0 +1,25 @@
+
+#app.rb
+
+puts "Hello World!"
+
+# This is the equivalent of calling:
+# puts [1, 2, 3].inspect
+# p [1, 2, 3]
+
+p [1, 2, 3]
+
+# The pp method will "pretty-print" these objects by calling the .pretty_inspect method on our data.
+
+# pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+
+ # type "irb" to get into irb
+
+ # In Ruby, it's convention to use underscores (_) to separate words in variables. This is referred to as snake case (as opposed to camel case, which is the convention in JavaScript).
+
+ # You can exit IRB by typing exit, or pressing ctrl + d.
+
+ # RSpec is a Ruby gem (the Ruby equivalent of a npm package) that provides a domain-specific language, or DSL
+
+ puts "Pass this test, please."
+
